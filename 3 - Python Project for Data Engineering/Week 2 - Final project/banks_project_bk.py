1613d98955d8d8620bc19fcd2961a96628d5bf97
Lam <lamlv723@gmail.com>
2024-02-27 02:34:34 +0700

    done

---
+++ 3 - Python Project for Data Engineering/Week 2 - Final project/banks_project_bk.py
@@ -0,0 +1,109 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+from datetime import datetime 
+
+def extract(url, table_attribs):
+      ''' This function aims to extract the required
+    information from the website and save it to a data frame. The
+    function returns the data frame for further processing. '''
+
+    page = requests.get(url).text
+    data = BeautifulSoup(page,'html.parser')
+    df = pd.DataFrame(columns=table_attribs)
+    tables = data.find_all('tbody')
+    rows = tables[2].find_all('tr')
+    for row in rows:
+        col = row.find_all('td')
+        if len(col)!=0:
+            if col[0].find('a') is not None and '—' not in col[2]:
+                data_dict = {"Name": col[0].a.contents[0],
+                             "MC_USD_Billion": col[2].contents[0]}
+                df1 = pd.DataFrame(data_dict, index=[0])
+                df = pd.concat([df,df1], ignore_index=True)
+    return df
+
+def transform(df, csv_path):
+    ''' This function accesses the CSV file for exchange rate
+    information, and adds three columns to the data frame, each
+    containing the transformed version of Market Cap column to
+    respective currencies'''
+
+    GDP_list = df["GDP_USD_millions"].tolist()
+    GDP_list = [float("".join(x.split(','))) for x in GDP_list]
+    GDP_list = [np.round(x/1000,2) for x in GDP_list]
+    df["GDP_USD_millions"] = GDP_list
+    df=df.rename(columns = {"GDP_USD_millions":"GDP_USD_billions"})
+    return df
+
+def load_to_csv(df, output_path):
+    ''' This function saves the final dataframe as a `CSV` file 
+    in the provided path. Function returns nothing.'''
+
+    df.to_csv(output_path)
+
+def load_to_db(df, sql_connection, table_name):
+    ''' This function saves the final dataframe to as a database table
+    with the provided name. Function returns nothing.'''
+
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
+
+def run_query(query_statement, sql_connection):
+    ''' This function runs the stated query on the database table and
+    prints the output on the terminal. Function returns nothing. '''
+
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
+
+def log_progress(message):
+    ''' This function logs the mentioned message of a given stage of the
+    code execution to a log file. Function returns nothing'''
+
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = datetime.now() # get current timestamp 
+    timestamp = now.strftime(timestamp_format) 
+    with open("./code_log.txt","a") as f: 
+        f.write(timestamp + ' : ' + message + '\n')    
+  
+''' Here, you define the required entities and call the relevant 
+functions in the correct order to complete the project. Note that this
+portion is not inside any function.'''
+
+url = 'https://web.archive.org/web/20230908091635/https://en.wikipedia.org/wiki/List_of_largest_banks'
+table_attribs = ["Name", "MC_USD_Billion"]
+db_name = 'Banks.db'
+table_name = 'Largest_banks'
+csv_path = './Largest_banks_data.csv'
+
+log_progress('Preliminaries complete. Initiating ETL process')
+
+df = extract(url, table_attribs)
+
+log_progress('Data extraction complete. Initiating Transformation process')
+
+df = transform(df)
+
+log_progress('Data transformation complete. Initiating loading process')
+
+load_to_csv(df, csv_path)
+
+log_progress('Data saved to CSV file')
+
+sql_connection = sqlite3.connect(db_name)
+
+log_progress('SQL Connection initiated')
+
+load_to_db(df, sql_connection, table_name)
+
+log_progress('Data loaded to Database as a table, Executing queries')
+
+query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+run_query(query_statement, sql_connection)
+
+log_progress('Process Complete')
+
+sql_connection.close()
+
+log_progress('Server Connection closed')
---
+++ test.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+from datetime import datetime 
+
+def extract(url, table_attribs):
+    ''' The purpose of this function is to extract the required
+    information from the website and save it to a dataframe. The
+    function returns the dataframe for further processing. '''
+
+    page = requests.get(url).text
+    data = BeautifulSoup(page,'html.parser')
+    df = pd.DataFrame(columns=table_attribs)
+    tables = data.find_all('tbody')
+    print(len(tables))
+    rows = tables[2].find_all('tr')
+    for row in rows:
+        col = row.find_all('td')
+        if len(col)!=0:
+            if col[0].find('a') is not None and '—' not in col[2]:
+                data_dict = {"Country": col[0].a.contents[0],
+                             "GDP_USD_millions": col[2].contents[0]}
+                df1 = pd.DataFrame(data_dict, index=[0])
+                df = pd.concat([df,df1], ignore_index=True)
+    return df
+  
+
+''' Here, you define the required entities and call the relevant 
+functions in the correct order to complete the project. Note that this
+portion is not inside any function.'''
+
+url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+table_attribs = ["Country", "GDP_USD_millions"]
+db_name = 'World_Economies.db'
+table_name = 'Countries_by_GDP'
+csv_path = './Countries_by_GDP.csv'
+
+
+df = extract(url, table_attribs)
